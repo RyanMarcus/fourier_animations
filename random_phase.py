b27c5f817a650687bb37c5e07b449dbd95e97b69
Ryan Marcus <ryan@rmarcus.info>
2016-10-10 17:52:27 -0400

    added brandeis image output

--- random_phase.py
+++ random_phase.py
@@ -10,7 +10,7 @@ def pm2i(mag, phase):
     return mag * np.exp(1j * phase)
 
 # mode = "L" means grayscale
-img = scipy.ndimage.imread("image.jpg", mode="L")
+img = scipy.ndimage.imread("brandeis.jpg", mode="L")
 fft = np.fft.rfft2(img)
 
 # clear the DC componenet (linear shift)
@@ -20,7 +20,7 @@ def gen_with_k_components(k):
     mag = np.abs(fft)
     phase = np.angle(fft)
 
-    largest_k_indices = np.argpartition(mag, -10, axis=None)[-10:]
+    largest_k_indices = np.argpartition(mag, -5, axis=None)[-5:]
 
     mask = np.zeros(fft.shape)
     for flat_idx in largest_k_indices:
@@ -38,7 +38,7 @@ def gen_image(inp):
     scipy.misc.imsave("frames/frame{}.png".format(idx), gen_with_k_components(i))
 
 def gen():
-    with Pool(4) as p:
+    with Pool(8) as p:
         values = enumerate(np.linspace(0, 2*math.pi, num=100))
         p.map(gen_image, values)
 
