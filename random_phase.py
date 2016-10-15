e8c76845f2a7876a771680997eee5816b6fd99d9
Arya Boudaie <Arya Boudaie>
2016-10-15 19:57:31 -0400

    No more global vars

--- random_phase.py
+++ random_phase.py
@@ -11,9 +11,6 @@ import glob
 import os
 
 
-# mode = "L" means grayscale
-img = scipy.ndimage.imread("flower.jpg")
-fft = [np.fft.rfft2(img[:,:,x]) for x in range(img.shape[2])]
 
 def file_key(path):
 		fname = os.path.split(path)[1]
@@ -24,7 +21,7 @@ def pm2i(mag, phase):
     return mag * np.exp(1j * phase)
 
 
-def gen_with_k_components(k):
+def gen_with_k_components(k, fft):
     toR = []
     for color_channel_fft in fft:
         mag = np.abs(color_channel_fft)
@@ -48,26 +45,34 @@ def gen_with_k_components(k):
         toR.append(ifft)
     return np.dstack(toR)
 
-def gen_image(inp):
+def gen_image(inp, fft):
     idx, i = inp
-    x = gen_with_k_components(i)
+    x = gen_with_k_components(i, fft)
     scipy.misc.imsave("frames/frame{}.png".format(idx), x)
 
-def gen():
+def gen(fft, num=100):
     with Pool(8) as p:
-        values = enumerate(np.linspace(0, 2*math.pi, num=100))
-        p.map(gen_image, values)
+        values = enumerate(np.linspace(0, 2*math.pi, num=num))
+        p.starmap(gen_image, zip(values, [fft]*num))
 
-if __name__=="__main__":
+def read_image_and_convert_gif(image_name):
     extractor = parallelTestModule.ParallelExtractor()
     extractor.runInParallel(numProcesses=2, numThreads=4)
 
+	# mode = "L" means grayscale
+    img = scipy.ndimage.imread(image_name)
+    fft = [np.fft.rfft2(img[:,:,x]) for x in range(img.shape[2])]
 
     # clear the DC componenet (linear shift)
     for color_channel in fft:
         color_channel[0][0] = 0
-    gen()
+    gen(fft)
+    print("Frames Generated: Generating GIF (Pronounced Jif)")
     with imageio.get_writer('movie.gif', mode='I') as writer:
         for filename in sorted(glob.glob("frames/*.png"), key=file_key):
             image = imageio.imread(filename)
             writer.append_data(image)
+			
+			
+if __name__=="__main__":
+    read_image_and_convert_gif('flower.jpg')
